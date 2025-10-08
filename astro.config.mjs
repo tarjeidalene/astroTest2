0280ed93142ee15682c86972454ea8e9e84d1f4b
tarjeidalene <173668059+tarjeidalene@users.noreply.github.com>
2025-10-08 12:56:08 +0200

    Steg 6a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrotest2tarjei.netlify.app/"
+  site: "https://astrotest2tarjei.netlify.app/",
+  integrations: [preact()]
 });
